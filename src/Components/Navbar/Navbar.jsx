10486d5e376dadf674e58995a5934ff410e0a021
Tanvin Ahmed <tanvinahmedtouhid91@gmail.com>
2021-03-20 22:09:08 +0600

    nav update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import { userContext } from "../../App";
 import logo from "../../img/bg/bg.png";
 import "./Navbar.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   const { loggedInUser, setLoggedInUser } = useContext(userContext);
@@ -13,14 +13,28 @@ const Navbar = () => {
     <nav>
       <input type="checkbox" id="check" />
       <label for="check" className="checkbtn">
-      <FontAwesomeIcon icon={faBars} />
+        <FontAwesomeIcon icon={faBars} />
       </label>
-      <Link to="/">  <label className="logo">City Riders</label></Link>
+      <Link to="/">
+        {" "}
+        <label className="logo">City Riders</label>
+      </Link>
       <ul>
         <li>
           <Link className="active" aria-current="page" to="/home">
             Home
           </Link>
+        </li>
+        <li>
+          {loggedInUser.email ? (
+            <Link className="active" aria-current="page" to="/">
+              Destination
+            </Link>
+          ) : (
+            <Link className="active" aria-current="page" to="/login">
+              Destination
+            </Link>
+          )}
         </li>
         <li>
           {loggedInUser.email ? (
@@ -38,11 +52,7 @@ const Navbar = () => {
           )}
         </li>
         <li>
-          {
-            <h5 className="text-light ml-4">
-              {loggedInUser.displayName}
-            </h5>
-          }
+          {<h5 className="text-light ml-4">{loggedInUser.displayName}</h5>}
         </li>
       </ul>
     </nav>
